27f444b20704a836129368f39e9a91bc61edf0b8
Rober Martín H <rmh.dev@gmail.com>
2012-04-17 21:04:07 +0200

    Exercise 13, with extra credit #3

---
+++ ex13-3.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first = argv
+
+print "The script is called: ", script
+print "Your first variable is: ", first
+value = raw_input("Enter a parameter: ")
+print "Your parameter is: ", value
